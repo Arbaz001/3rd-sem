e596771a0fec9d1d718cc0f3865d68891ae9febe
ARBAZ ALI <107638812+Arbaz001@users.noreply.github.com>
2023-01-02 22:51:03 +0530

    Simple Array Sum

---
+++ Assignment 3/Simple Array Sum.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+int sum=0;
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++){
+    cin>>arr[i];
+}
+for(int i=0;i<n;i++){
+    
+   sum+=arr[i];
+   
+    
+}cout<<sum;
+return 0;
+}
