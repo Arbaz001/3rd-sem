35c01de5b2d78d38c2f6e3ae5083da2cb33e986e
ARBAZ ALI <107638812+Arbaz001@users.noreply.github.com>
2023-01-02 22:42:00 +0530

    Update Assignment 3 .cpp

--- Assignment 3 .cpp
+++ Assignment 3 .cpp
@@ -263,131 +263,34 @@ for(i=0; i<5; i++)
         return 0;
 }
 
-Q6 coompare the triplets
-//worked at only in fuction
+Q6 compare the triplets
 #include <bits/stdc++.h>
 
 using namespace std;
-
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
-
-/*
- * Complete the 'compareTriplets' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY a
- *  2. INTEGER_ARRAY b
- */
-
-vector<int> compareTriplets(vector<int> vec1, vector<int> vec2) {
-vector<int>newvec;
-int alice=0;
-int bob=0;
-
+int main(){
+    int alice=0;
+    int bob=0;
+    int a[3],b[3];
     for(int i=0;i<3;i++){
-      
-           if(vec1[i]>vec2[i]){
-             alice++;
-           }
-           else if(vec1[i]<vec2[i]){
-                bob++;
-           }
+        cin>>a[i];
     }
-   newvec.push_back(alice);
-   newvec.push_back(bob);
-  return newvec;
-}
-
-
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string a_temp_temp;
-    getline(cin, a_temp_temp);
-
-    vector<string> a_temp = split(rtrim(a_temp_temp));
-
-    vector<int> a(3);
-
-    for (int i = 0; i < 3; i++) {
-        int a_item = stoi(a_temp[i]);
-
-        a[i] = a_item;
+    for(int i=0;i<3;i++){
+        cin>>b[i];
     }
-
-    string b_temp_temp;
-    getline(cin, b_temp_temp);
-
-    vector<string> b_temp = split(rtrim(b_temp_temp));
-
-    vector<int> b(3);
-
-    for (int i = 0; i < 3; i++) {
-        int b_item = stoi(b_temp[i]);
-
-        b[i] = b_item;
-    }
-
-    vector<int> result = compareTriplets(a, b);
-
-    for (size_t i = 0; i < result.size(); i++) {
-        fout << result[i];
-
-        if (i != result.size() - 1) {
-            fout << " ";
+    for(int i=0;i<3;i++){
+        if(a[i]>b[i]){
+            alice++;
+           
         }
-    }
-
-    fout << "\n";
-
-    fout.close();
-
+        else if(a[i]<b[i]){
+            bob++;
+            
+        }
+    } 
+     cout<<alice<<" ";
+     cout<<bob<<" ";
     return 0;
 }
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
-}
-
 
 Q7 staircase
 
