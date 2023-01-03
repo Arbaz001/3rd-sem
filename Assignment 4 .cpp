bfdb7dbd7b0c421b66de5c407d7ff7c312e5de65
ARBAZ ALI <107638812+Arbaz001@users.noreply.github.com>
2023-01-03 22:41:48 +0530

    implementation

--- Assignment 4 .cpp
+++ Assignment 4 .cpp
@@ -65,26 +65,39 @@ int main() {
 
 
     Q3.APPLE AND ORANGE
+   #include<bits/stdc++.h>
+  using namespace std;
+  int main(){
+   int sum=0;
+   int rum=0;
+    int s,t,a,b,m,n;
+    cin>>s>>t>>a>>b>>m>>n;
+    int A[m],O[n];
+    for(int i=0;i<m;i++)
+    {
+        cin>>A[i];
+        A[i]=a+A[i];
+        if(A[i]>=s && A[i]<=t)
+        {
+            rum++;
+        }
+        
+    }
+    cout<<rum<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>O[i];
+        O[i]=b+O[i];
+        if(O[i]>=s && O[i]<=t)
+        {
+            sum++;
+        }
+        
+        
+        
+    }
+    cout<<sum<<endl;
     
-    #include <bits/stdc++.h>
-using namespace std;
-int main() {
-    int s, t, a, b, n, m, d, ans1=0, ans2=0;
-    cin >> s >> t >> a >> b >> m >> n;
-    for(int i=0;i<m; i++) {
-        cin>>d;
-        d = a+d;
-        if(d>=s && d<=t)
-            ans1++;
-    }
-    for(int i=0;i<n; i++) {
-        cin>>d;
-        d = b+d;
-        if(d>=s && d<=t)
-            ans2++;
-    }
-    cout << ans1 << endl;
-    cout << ans2 << endl; 
     return 0;
 }
 
